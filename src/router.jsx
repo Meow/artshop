9e409b5cb2200fdaec17b6eb3f8bea2f7d81f456
Luna D. <luna@nighty.cloud>
2024-02-20 14:25:00 +0100

    skeleton structure

---
+++ src/router.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./routes/root";
+import Artists from "./routes/artists";
+import Catalogue from "./routes/catalogue";
+import Gallery from "./routes/gallery";
+
+export default createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/artists",
+    element: <Artists />,
+  },
+  {
+    path: "/catalogue",
+    element: <Catalogue />,
+  },
+  {
+    path: "/gallery",
+    element: <Gallery />,
+  },
+]);
